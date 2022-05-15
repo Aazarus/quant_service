f83e9e54b0016441d7253fe270c5355cfc4cdd6b
Aaron <aaron@sevnasoftware.com>
2022-05-15 23:12:20 +0100

    Updated DbInitialiser to only process what is needed and reduce number of saves.

--- Service/Service/Models/DbInitialiser.cs
+++ Service/Service/Models/DbInitialiser.cs
@@ -15,8 +15,13 @@ public class DbInitialiser
 
         context.Database.EnsureCreated();
 
-        if (context.Symbols != null && context.IndexData != null && context.IndexData.Any() &&
-            context.Symbols.Any())
+        if (context.Symbols != null &&
+            context.IndexData != null &&
+            context.Prices != null &&
+            context.IndexData.Any() &&
+            context.Symbols.Any() &&
+            context.Prices.Any()
+           )
             return;
 
         string path = AppContext.BaseDirectory;
@@ -25,19 +30,27 @@ public class DbInitialiser
 
         if (!Directory.Exists(filePath)) throw new DirectoryNotFoundException("Model Helper directory not found.");
 
-        var data = ModelHelper.CsvToIndexData(filePath + "Indices.csv");
+        if (context.Symbols != null && !context.Symbols.Any())
+        {
+            var symbols = ModelHelper.CsvToSymbolList(filePath + "StockTickers.csv");
+            foreach (var symbol in symbols) context.Symbols?.Add(symbol);
 
-        foreach (var d in data) context.IndexData?.Add(d);
+            // Prices relies on Symbol so need to ensure this is processed first.
+            context.SaveChanges();
+        }
 
-        context.SaveChanges();
+        if (context.IndexData != null && !context.IndexData.Any())
+        {
+            var data = ModelHelper.CsvToIndexData(filePath + "Indices.csv");
 
-        var symbols = ModelHelper.CsvToSymbolList(filePath + "StockTickers.csv");
-        foreach (var symbol in symbols) context.Symbols?.Add(symbol);
+            foreach (var d in data) context.IndexData?.Add(d);
+        }
 
-        context.SaveChanges();
-
-        var prices = ModelHelper.CsvToIbmPrices("IBM", context, filePath + "IBM.csv");
-        foreach (var price in prices) context.Prices?.Add(price);
+        if (context.Prices != null && !context.Prices.Any())
+        {
+            var prices = ModelHelper.CsvToIbmPrices("IBM", context, filePath + "IBM.csv");
+            foreach (var price in prices) context.Prices?.Add(price);
+        }
 
         context.SaveChanges();
     }
