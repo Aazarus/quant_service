0a3ebe4a30bf68400d3751242940035f67aac863
Aaron <aaron@sevnasoftware.com>
2022-05-15 23:11:54 +0100

    Updated ModelHelper.cs to be more flexible with potential issues

--- Service/Service/Helpers/ModelHelper.cs
+++ Service/Service/Helpers/ModelHelper.cs
@@ -4,8 +4,10 @@
 
 namespace Service.Helpers;
 
+using System.Diagnostics.CodeAnalysis;
 using Models;
 
+[ExcludeFromCodeCoverage]
 public class ModelHelper
 {
     public static List<Symbol> CsvToSymbolList(string csvFile)
@@ -13,20 +15,19 @@ public class ModelHelper
         //string csvFile = @"..\Models\StockTickers.csv";
         var fs = new FileStream(csvFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         var sr = new StreamReader(fs);
-        var lst = new List<string>();
+        var lst = new List<string?>();
         while (!sr.EndOfStream)
             lst.Add(sr.ReadLine());
 
-        string[] fields = lst[0].Split(new[] {','});
         var res = new List<Symbol>();
         for (var i = 1; i < lst.Count; i++)
         {
-            fields = lst[i].Split(',');
+            string[]? fields = lst[i]?.Split(',');
             res.Add(new Symbol
             {
-                Ticker = fields[0],
-                Region = fields[1],
-                Sector = fields[2]
+                Ticker = fields?[0] ?? string.Empty,
+                Region = fields?[1] ?? string.Empty,
+                Sector = fields?[2] ?? string.Empty
             });
         }
 
@@ -38,22 +39,21 @@ public class ModelHelper
         //string csvFile = @"..\Models\Data\indices.csv";
         var fs = new FileStream(csvFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         var sr = new StreamReader(fs);
-        var lst = new List<string>();
+        var lst = new List<string?>();
         while (!sr.EndOfStream)
             lst.Add(sr.ReadLine());
 
-        string[] fields = lst[0].Split(new[] {','});
         var res = new List<IndexData>();
         for (var i = 1; i < lst.Count; i++)
         {
-            fields = lst[i].Split(',');
+            string?[]? fields = lst[i]?.Split(',');
             res.Add(new IndexData
             {
-                Date = DateTime.Parse(fields[0]),
-                IGSpread = decimal.Parse(fields[1]),
-                HYSpread = decimal.Parse(fields[2]),
-                SPX = decimal.Parse(fields[3]),
-                VIX = decimal.Parse(fields[4])
+                Date = DateTime.Parse(fields?[0] ?? string.Empty),
+                IGSpread = decimal.Parse(fields?[1] ?? string.Empty),
+                HYSpread = decimal.Parse(fields?[2] ?? string.Empty),
+                SPX = decimal.Parse(fields?[3] ?? string.Empty),
+                VIX = decimal.Parse(fields?[4] ?? string.Empty)
             });
         }
 
@@ -65,25 +65,24 @@ public class ModelHelper
         int symbolId = TickerToId(ticker, context);
         var fs = new FileStream(csvFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         var sr = new StreamReader(fs);
-        var lst = new List<string>();
+        var lst = new List<string?>();
         while (!sr.EndOfStream)
             lst.Add(sr.ReadLine());
 
-        string[] fields = lst[0].Split(new[] {','});
         var res = new List<Price>();
         for (var i = 1; i < lst.Count; i++)
         {
-            fields = lst[i].Split(',');
+            string[]? fields = lst[i]?.Split(',');
             res.Add(new Price
             {
                 SymbolId = symbolId,
-                Date = DateTime.Parse(fields[0]),
-                Open = decimal.Parse(fields[1]),
-                High = decimal.Parse(fields[2]),
-                Low = decimal.Parse(fields[3]),
-                Close = decimal.Parse(fields[4]),
-                CloseAdj = decimal.Parse(fields[5]),
-                Volume = decimal.Parse(fields[6])
+                Date = DateTime.Parse(fields?[0] ?? string.Empty),
+                Open = decimal.Parse(fields?[1] ?? string.Empty),
+                High = decimal.Parse(fields?[2] ?? string.Empty),
+                Low = decimal.Parse(fields?[3] ?? string.Empty),
+                Close = decimal.Parse(fields?[4] ?? string.Empty),
+                CloseAdj = decimal.Parse(fields?[5] ?? string.Empty),
+                Volume = decimal.Parse(fields?[6] ?? string.Empty)
             });
         }
 
@@ -93,7 +92,6 @@ public class ModelHelper
     public static int TickerToId(string ticker, QuantDataContext context)
     {
         var id = 0;
-        var symbol = new Symbol();
         var query = from s in context.Symbols
             where s.Ticker == ticker
             select s.SymbolId;
