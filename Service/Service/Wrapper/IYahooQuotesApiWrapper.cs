d58666d23f75e3d1910673110c486c01963fbcfc
Aaron <aaron@sevnasoftware.com>
2022-06-09 21:54:52 +0100

    Added a warpper for YahooQuotesApi

---
+++ Service/Service/Wrapper/IYahooQuotesApiWrapper.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IYahooQuotesApiWrapper.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Wrapper;
+
+using NodaTime;
+using YahooQuotesApi;
+
+public interface IYahooQuotesApiWrapper
+{
+    /// <summary>
+    ///     Gets a Security by its ticker and gets the Price History by the frequency given and starting at the given start
+    ///     date.
+    /// </summary>
+    /// <param name="ticker">The ticker for the security.</param>
+    /// <param name="start">The start date for the price history.</param>
+    /// <param name="frequency">The frequency for the price ticks.</param>
+    /// <returns>A collection of PriceTicks.</returns>
+    Task<IEnumerable<PriceTick>?> GetSecurityPriceHistoryAsync(string ticker, Instant start, Frequency frequency);
+}
---
+++ Service/Service/Wrapper/YahooQuotesApiWrapper.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="YahooQuotesApiWrapper.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Wrapper;
+
+using System.Diagnostics.CodeAnalysis;
+using NodaTime;
+using YahooQuotesApi;
+
+/// <summary>
+///     Provides a wrapper for the YahooQuotesApi library.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class YahooQuotesApiWrapper : IYahooQuotesApiWrapper
+{
+    /// <inheritdoc />
+    public async Task<IEnumerable<PriceTick>?> GetSecurityPriceHistoryAsync(string ticker, Instant start,
+        Frequency frequency)
+    {
+        var yahooService = await CreateYahooQuotesWithStartAndFrequency(start, frequency)
+            .GetAsync(ticker, HistoryFlags.PriceHistory);
+
+        return yahooService?.PriceHistory.Value;
+    }
+
+    /// <summary>
+    ///     Creates a YahooQuotes object to collect Price history from the given start date and for the given frequency.
+    /// </summary>
+    /// <param name="start">The start date for the price history.</param>
+    /// <param name="frequency">The frequency for the price ticks.</param>
+    /// <returns>A YahooQuotes object.</returns>
+    private static YahooQuotes CreateYahooQuotesWithStartAndFrequency(Instant start, Frequency frequency)
+    {
+        return new YahooQuotesBuilder()
+            .WithHistoryStartDate(start)
+            .WithPriceHistoryFrequency(frequency)
+            .Build();
+    }
+}
