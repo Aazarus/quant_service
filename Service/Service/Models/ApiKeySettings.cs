628a1688a0d78e5bef74b9681edd9a3f2c9ba73f
Aaron <aaron@sevnasoftware.com>
2022-06-24 20:16:47 +0100

    Added new class for storing ApiKeys with IEX keys being the first.

---
+++ Service/Service/Models/ApiKeySettings.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ApiKeySettings.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Models;
+
+public class ApiKeySettings
+{
+    /// <summary>
+    ///     Defines the class holding the API Key for IEX access.
+    /// </summary>
+    public class IEX
+    {
+        /// <summary>
+        ///     Gets or sets the Publishable Token for IEX access.
+        /// </summary>
+        public string PublishableToken { get; set; } = "";
+
+        /// <summary>
+        ///     Gets or sets the Security Token for IEX access.
+        /// </summary>
+        public string SecurityToken { get; set; } = "";
+    }
+}
