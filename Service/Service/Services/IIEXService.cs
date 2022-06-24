8fa7b236c35975bdf652d775ab17285bb434aa4e
Aaron <aaron@sevnasoftware.com>
2022-06-24 20:40:13 +0100

    Added wrapper interface for IEX service

---
+++ Service/Service/Services/IIEXService.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IIEXService.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Services;
+
+using IEXSharp.Model.CoreData.StockPrices.Request;
+using Models;
+
+public interface IIEXService
+{
+    /// <summary>
+    ///     Gets the IEX Stock data for a given ticker within the provided range.
+    /// </summary>
+    /// <param name="ticker">The Stock Ticker.</param>
+    /// <param name="range">The data range.</param>
+    /// <returns>A collection of stock data.</returns>
+    Task<List<StockData>> GetStock(string ticker,
+        ChartRange range);
+}
