88fb471ead1cd893d458e9dc27ddf547bd86f691
Aaron <aaron@sevnasoftware.com>
2022-06-24 19:44:18 +0100

    Renamed file and added missing test assert and supporting code

--- Service/Service.Tests/Services/YahooServiceTests.cs
+++ Service/Service.Tests/Services/YahooServiceTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="YahooServiceTest.cs" company="Sevna Software LTD">
+﻿// <copyright file="YahooServiceTests.cs" company="Sevna Software LTD">
 // Copyright (c) Sevna Software LTD. All rights reserved.
 // </copyright>
 
@@ -6,10 +6,12 @@ namespace Service.Tests.Services;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Data;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
+using Models;
 using Moq;
 using NodaTime;
 using Service.Services;
@@ -17,13 +19,13 @@ using Wrapper;
 using Xunit;
 using YahooQuotesApi;
 
-public class YahooServiceTest
+public class YahooServiceTests
 {
     private readonly Mock<IYahooQuotesApiWrapper> _apiWrapper;
     private readonly Mock<ILogger<YahooService>> _logger;
     private YahooService? _service;
 
-    public YahooServiceTest()
+    public YahooServiceTests()
     {
         _logger = new Mock<ILogger<YahooService>>();
         _apiWrapper = new Mock<IYahooQuotesApiWrapper>();
@@ -70,11 +72,13 @@ public class YahooServiceTest
         var end = DateTime.UtcNow.AddDays(30);
         const string period = "daily";
 
+        var expected = ConvertPriceTickToStockDataCollection(ticker, TestData.SecurityPriceHistory);
+
         // Act
-        await _service.GetStockDataWithPrices(ticker, start, end, period);
+        var actual = await _service.GetStockDataWithPrices(ticker, start, end, period);
 
         // Assert
-        // ToDo: Confirm the result
+        actual.Should().BeEquivalentTo(expected);
     }
 
     [Fact]
@@ -114,5 +118,22 @@ public class YahooServiceTest
 
         // Assert
         actual.Should().Be(Frequency.Daily);
+    }
+
+    private static IEnumerable<StockData> ConvertPriceTickToStockDataCollection(string ticker,
+        IEnumerable<PriceTick> priceTicks)
+    {
+        return priceTicks.Select(priceTick => new StockData
+            {
+                Ticker = ticker,
+                Date = priceTick.Date.ToDateTimeUnspecified(),
+                Open = Convert.ToDecimal(priceTick.Open),
+                High = Convert.ToDecimal(priceTick.High),
+                Low = Convert.ToDecimal(priceTick.Low),
+                Close = Convert.ToDecimal(priceTick.Close),
+                CloseAdj = Convert.ToDecimal(priceTick.AdjustedClose),
+                Volume = Convert.ToDecimal(priceTick.Volume)
+            })
+            .ToList();
     }
 }
