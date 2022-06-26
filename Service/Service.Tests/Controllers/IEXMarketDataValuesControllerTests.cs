9ab02f6cd676694dbab13d991d2c7978f43a478b
Aaron <aaron@sevnasoftware.com>
2022-06-26 21:54:32 +0100

    Added new API endpoint for getting stock data from IEX, inc tests

---
+++ Service/Service.Tests/Controllers/IEXMarketDataValuesControllerTests.cs
@@ -0,0 +1,127 @@
+﻿// <copyright file="IEXMarketDataValuesControllerTests.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Tests.Controllers;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using FluentAssertions;
+using IEXSharp.Model.CoreData.StockPrices.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using Moq;
+using Service.Controllers;
+using Service.Services;
+using Xunit;
+
+public class IEXMarketDataValuesControllerTests
+{
+    private readonly Mock<IIEXService> _iexService;
+    private readonly Mock<ILogger<IEXMarketDataValuesController>> _logger;
+    private IEXMarketDataValuesController _controller = null!;
+
+    public IEXMarketDataValuesControllerTests()
+    {
+        _logger = new Mock<ILogger<IEXMarketDataValuesController>>();
+        _iexService = new Mock<IIEXService>();
+    }
+
+    [Fact]
+    public async Task GetIexStock_ShouldReturnBadRequestForNullTicker()
+    {
+        // Arrange
+        _controller = new IEXMarketDataValuesController(_logger.Object, _iexService.Object);
+
+        // Act
+        var actual = await _controller.GetIexStock(null!);
+
+        // Assert
+        actual.Should().BeOfType(typeof(BadRequestObjectResult));
+
+        var actualObj = actual as BadRequestObjectResult;
+        actualObj.Should().NotBeNull();
+        actualObj!.StatusCode.Should().Be(400);
+        actualObj.Value.Should().Be("Ticker is invalid");
+    }
+
+    [Fact]
+    public async Task GetIexStock_ShouldReturnBadRequestForEmptyTicker()
+    {
+        // Arrange
+        _controller = new IEXMarketDataValuesController(_logger.Object, _iexService.Object);
+
+        // Act
+        var actual = await _controller.GetIexStock(string.Empty);
+
+        // Assert
+        actual.Should().BeOfType(typeof(BadRequestObjectResult));
+
+        var actualObj = actual as BadRequestObjectResult;
+        actualObj.Should().NotBeNull();
+        actualObj!.StatusCode.Should().Be(400);
+        actualObj.Value.Should().Be("Ticker is invalid");
+    }
+
+    [Fact]
+    public async Task GetIexStock_ShouldReturnBadRequestForWhitespaceTicker()
+    {
+        // Arrange
+        _controller = new IEXMarketDataValuesController(_logger.Object, _iexService.Object);
+
+        // Act
+        var actual = await _controller.GetIexStock(" ");
+
+        // Assert
+        actual.Should().BeOfType(typeof(BadRequestObjectResult));
+
+        var actualObj = actual as BadRequestObjectResult;
+        actualObj.Should().NotBeNull();
+        actualObj!.StatusCode.Should().Be(400);
+        actualObj.Value.Should().Be("Ticker is invalid");
+    }
+
+    [Fact]
+    public async Task GetIexStock_ShouldReturnNotFoundForEmptyCollectionFromService()
+    {
+        // Arrange
+        _iexService.Setup(s => s.GetStock(It.IsAny<string>(), It.IsAny<ChartRange>()))
+            .ReturnsAsync(await Task.FromResult(new List<StockData>()));
+        _controller = new IEXMarketDataValuesController(_logger.Object, _iexService.Object);
+
+        // Act
+        var actual = await _controller.GetIexStock("IBM");
+
+        // Assert
+        actual.Should().BeOfType(typeof(NotFoundObjectResult));
+
+        var actualObj = actual as NotFoundObjectResult;
+        actualObj.Should().NotBeNull();
+        actualObj!.StatusCode.Should().Be(404);
+        actualObj.Value.Should().Be("No data for Ticker: IBM");
+    }
+
+    [Fact]
+    public async Task GetIexStock_ShouldReturnOKWithDataFromService()
+    {
+        // Arrange
+        var expected = TestData.StockDataDaily.ToList();
+        _iexService.Setup(s => s.GetStock(It.IsAny<string>(), It.IsAny<ChartRange>()))
+            .ReturnsAsync(await Task.FromResult(expected));
+        _controller = new IEXMarketDataValuesController(_logger.Object, _iexService.Object);
+
+        // Act
+        var actual = await _controller.GetIexStock("IBM");
+
+        // Assert
+        actual.Should().BeOfType(typeof(OkObjectResult));
+
+        var actualObj = actual as OkObjectResult;
+        actualObj.Should().NotBeNull();
+        actualObj!.StatusCode.Should().Be(200);
+        actualObj.Value.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Service/Service/Controllers/IEXMarketDataValuesController.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="IEXMarketDataValuesController.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Controllers;
+
+using IEXSharp.Model.CoreData.StockPrices.Request;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+[Route("api/IexStock")]
+public class IEXMarketDataValuesController : ControllerBase
+{
+    /// <summary>
+    ///     The IEXService instance.
+    /// </summary>
+    private readonly IIEXService _iexService;
+
+    /// <summary>
+    ///     The application logger.
+    /// </summary>
+    private readonly ILogger<IEXMarketDataValuesController> _logger;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="IEXMarketDataValuesController" /> class.
+    /// </summary>
+    /// <param name="logger">The application logger.</param>
+    /// <param name="iexService">The IEX API Service</param>
+    public IEXMarketDataValuesController(
+        ILogger<IEXMarketDataValuesController> logger,
+        IIEXService iexService)
+    {
+        _logger = logger;
+        _iexService = iexService;
+    }
+
+    /// <summary>
+    ///     Gets the IEX Stock data for a given ticker within the provided range.
+    /// </summary>
+    /// <param name="ticker">The Stock Ticker.</param>
+    /// <param name="range">The data range.</param>
+    /// <returns>An IActionResult.</returns>
+    [Route("{ticker}/{range}")]
+    [HttpGet]
+    public async Task<IActionResult> GetIexStock(
+        string ticker,
+        ChartRange range =
+            ChartRange.OneMonth
+    )
+    {
+        if (string.IsNullOrWhiteSpace(ticker)) return BadRequest("Ticker is invalid");
+
+        var data = await _iexService.GetStock(ticker, range);
+
+        if (!data.Any()) return NotFound($"No data for Ticker: {ticker}");
+
+        return Ok(data);
+    }
+}
