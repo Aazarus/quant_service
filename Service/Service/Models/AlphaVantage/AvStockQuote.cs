d913bbc765c10795ab28e50e324613f99892c766
Aaron <aaron@sevnasoftware.com>
2022-08-14 22:15:19 +0100

    Added new AvStockQuote model

---
+++ Service/Service/Models/AlphaVantage/AvStockQuote.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AvStockQuote.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Models.AlphaVantage;
+
+public class AvStockQuote
+{
+    public string? Ticker { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public decimal Open { get; set; }
+    public decimal High { get; set; }
+    public decimal Low { get; set; }
+    public decimal Price { get; set; }
+    public decimal Volume { get; set; }
+    public decimal PrevClose { get; set; }
+    public decimal Change { get; set; }
+    public decimal ChangePercent { get; set; }
+}
