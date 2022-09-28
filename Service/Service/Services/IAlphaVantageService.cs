2d8e5846d9075161964fb5197e06191343e36399
Aaron <aaron@sevnasoftware.com>
2022-09-28 23:15:18 +0100

    Added new method to IAlphaVantageService

--- Service/Service/Services/IAlphaVantageService.cs
+++ Service/Service/Services/IAlphaVantageService.cs
@@ -5,13 +5,14 @@
 namespace Service.Services;
 
 using Models;
+using Models.AlphaVantage;
 
 public interface IAlphaVantageService
 {
     /// <summary>
     ///     Gets the End Of Day (EOD) data for a given ticker between the start and end dates for a given period of time.
     /// </summary>
-    /// <param name="ticker">The Stock Ticker.</param>
+    /// <param name="ticker">The stock Ticker.</param>
     /// <param name="start">The start Date.</param>
     /// <param name="end">The end Date.</param>
     /// <param name="period">The period.</param>
@@ -21,9 +22,16 @@ public interface IAlphaVantageService
     /// <summary>
     ///     Get the Bar data for a given ticker with a set interval (time period) and output size.
     /// </summary>
-    /// <param name="ticker">The Stock Ticker.</param>
+    /// <param name="ticker">The stock Ticker.</param>
     /// <param name="interval">The time interval.</param>
     /// <param name="outputSize">The output size.</param>
     /// <returns>List of StockData.</returns>
     Task<List<StockData>> GetStockBar(string ticker, int interval, int outputSize);
+
+    /// <summary>
+    ///     Get the latest stock data for a given ticker.
+    /// </summary>
+    /// <param name="ticker">The stock Ticker.</param>
+    /// <returns>An AvStockQuote object.</returns>
+    Task<AvStockQuote> GetStockQuote(string ticker);
 }
