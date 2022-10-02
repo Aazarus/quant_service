ffbab340beff4230e61da46a2faca7df174c7efd
Aaron <aaron@sevnasoftware.com>
2022-10-02 20:44:10 +0100

    Removed unnecessary property 'volume' from AvFxData

--- Service/Service/Models/AlphaVantage/AvFxData.cs
+++ Service/Service/Models/AlphaVantage/AvFxData.cs
@@ -15,5 +15,4 @@ public class AvFxData
     public decimal High { get; set; }
     public decimal Low { get; set; }
     public decimal Close { get; set; }
-    public decimal Volume { get; set; }
 }
