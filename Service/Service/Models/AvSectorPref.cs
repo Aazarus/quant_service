511824ad2816c9775a8190db75fb38c03d9e217b
Aaron <aaron@sevnasoftware.com>
2022-10-04 22:21:59 +0100

    Added new AvSectorRank model

---
+++ Service/Service/Models/AvSectorPref.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="AvSectorPref.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Models;
+
+using System.Diagnostics.CodeAnalysis;
+
+[ExcludeFromCodeCoverage]
+public class AvSectorPref
+{
+    public string Rank { get; set; }
+    public string CommunicationServices { get; set; }
+    public string ConsumerDiscretionary { get; set; }
+    public string ConsumerStaples { get; set; }
+    public string Energy { get; set; }
+    public string Financials { get; set; }
+    public string HealthCare { get; set; }
+    public string Industrials { get; set; }
+    public string InformationTechnology { get; set; }
+    public string Materials { get; set; }
+    public string Utilities { get; set; }
+}
