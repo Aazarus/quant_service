e69014e9c6202efd10701e2c68cbe8d543e75519
Aaron <aaron@sevnasoftware.com>
2023-02-20 00:01:50 +0000

    Added new wrapper for Quandl API wrapper

---
+++ Service/Service/Wrapper/IQuandlWrapper.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IQuandlWrapper.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Wrapper;
+
+using Quandl.NET.Model.Response;
+
+public interface IQuandlWrapper
+{
+    /// <summary>
+    ///     Gets the stock data for a given ticker from Quandl.
+    /// </summary>
+    /// <param name="apiKey">The Quandl ApiKey.</param>
+    /// <param name="ticker">The ticker.</param>
+    /// <param name="start">The start date for the data.</param>
+    /// <param name="end">The end date for the data.</param>
+    /// <returns>TimeseriesDataResponse.</returns>
+    Task<TimeseriesDataResponse> GetQuandlStock(string apiKey, string ticker, string start, string end);
+}
---
+++ Service/Service/Wrapper/QuandlWrapper.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="QuandlWrapper.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Wrapper;
+
+using Quandl.NET;
+using Quandl.NET.Model.Response;
+
+public class QuandlWrapper : IQuandlWrapper
+{
+    /// <inheritdoc />
+    public async Task<TimeseriesDataResponse> GetQuandlStock(string apiKey, string ticker, string start, string end)
+    {
+        var startDate = DateTime.Parse(start);
+        var endDate = DateTime.Parse(end);
+        var client = new QuandlClient(apiKey);
+
+        return await client.Timeseries.GetDataAsync("WIKI", ticker, startDate: startDate, endDate: endDate);
+    }
+}
