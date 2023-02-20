d8eaa51cd36bc97b9f0e0fd34f5d061d7ea45b21
Aaron <aaron@sevnasoftware.com>
2023-02-20 00:02:17 +0000

    Added new Quandl Stock Data model

---
+++ Service/Service/Models/Quandl/QuandlStockData.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="QuandlStockData.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Models.Quandl;
+
+public class QuandlStockData
+{
+    /// <summary>
+    ///     Gets or sets the Ticker.
+    /// </summary>
+    public string? Ticker { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Stock Data Date.
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Open price.
+    /// </summary>
+    public decimal Open { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the High price.
+    /// </summary>
+    public decimal High { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Low price.
+    /// </summary>
+    public decimal Low { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Close price.
+    /// </summary>
+    public decimal Close { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Volume.
+    /// </summary>
+    public decimal Volume { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Open adjusted price.
+    /// </summary>
+    public decimal OpenAdj { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the High adjusted price.
+    /// </summary>
+    public decimal HighAdj { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Low adjusted price.
+    /// </summary>
+    public decimal LowAdj { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Close adjusted price.
+    /// </summary>
+    public decimal CloseAdj { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Volume adjusted price.
+    /// </summary>
+    public decimal VolumeAdj { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the ExDividend price.
+    /// </summary>
+    public decimal ExDividend { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the split ratio.
+    /// </summary>
+    public decimal SplitRatio { get; set; }
+}
