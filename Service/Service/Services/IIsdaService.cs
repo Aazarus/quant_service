f5e98931742c53b73dd2ed1965429404e1db5fa4
Aaron <aaron@sevnasoftware.com>
2023-02-21 19:22:05 +0000

    Added API service for ISDA

---
+++ Service/Service/Services/IIsdaService.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IIsdaService.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Services;
+
+using Models.ISDA;
+
+public interface IIsdaService
+{
+    /// <summary>
+    ///     Gets the ISDA rates for a given currency.
+    /// </summary>
+    /// <param name="currency">The currency to check.</param>
+    /// <param name="date">The date for the check.</param>
+    /// <returns>A collection of IsdaRate objects.</returns>
+    Task<IEnumerable<IsdaRate>> GetIsdaRates(string currency, string date);
+}
---
+++ Service/Service/Services/IsdaService.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="IsdaService.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Services;
+
+using Models.ISDA;
+using Wrapper;
+
+public class IsdaService : IIsdaService
+{
+    /// <summary>
+    ///     The ISDA wrapper.
+    /// </summary>
+    private readonly IIsdaWrapper _apiWrapper;
+
+    /// <summary>
+    ///     The application logger.
+    /// </summary>
+    private readonly ILogger<IsdaService> _logger;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="IsdaService" /> class.
+    /// </summary>
+    /// <param name="logger">The Application Logger.</param>
+    /// <param name="apiWrapper">The ISDA Api Wrapper.</param>
+    public IsdaService(ILogger<IsdaService> logger, IIsdaWrapper apiWrapper)
+    {
+        _logger = logger;
+        _apiWrapper = apiWrapper;
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<IsdaRate>> GetIsdaRates(string currency, string date)
+    {
+        return await _apiWrapper.GetIsdaRates(currency, date);
+    }
+}
