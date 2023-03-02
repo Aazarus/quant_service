2aafca981395cb8fe037f5d301323c334b012d32
Aaron <aaron@sevnasoftware.com>
2023-03-02 21:44:04 +0000

    Added new model StockOutput

---
+++ Service/Service/Models/StockOutput.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="StockOutput.cs" company="Sevna Software LTD">
+// Copyright (c) Sevna Software LTD. All rights reserved.
+// </copyright>
+
+namespace Service.Models;
+
+public class StockOutput
+{
+    /// <summary>
+    ///     Gets or sets the status of the connection.
+    /// </summary>
+    public string? Status { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the StockData.
+    /// </summary>
+    public StockData? Stock { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Total Data Points from the endpoint.
+    /// </summary>
+    public int TotalDataPoints { get; set; }
+}
